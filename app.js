e061b3db98256931a7b1478cfde367aa43ba93cf
Yaroslav Melnichuk <nemish.i.nelos@gmail.com>
2017-10-03 14:06:44 +0300

    another try of socket 5

--- app.js
+++ app.js
@@ -47,6 +47,12 @@ const passportConfig = require('./config/passport');
  * Create Express server.
  */
 const app = express();
+app.use(function(request, response, next){
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token");
+    next();
+});
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "X-Requested-With");
