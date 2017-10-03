59864327da6e06c34182af5df90956f3140bd59a
Yaroslav Melnichuk <nemish.i.nelos@gmail.com>
2017-10-03 03:00:40 +0300

    another try of socket 2

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,12 @@
 import io from 'socket.io-client';
-const HOST = window.location.protocol + '//' + window.location.hostname + ':3002';
+let secure = false;
+let { protocol } = window.location;
+console.log('choosing protocol', protocol)
+if (window.location.protocol == 'https:') {
+    secure = true;
+    protocol = 'wss:'
+    console.log('secure protocol', protocol)
+}
+const HOST = protocol + '//' + window.location.hostname + ':3002';
 // export default io(HOST, {secure: window.location.protocol === 'https:'});
-export default io(HOST, {secure: window.location.protocol === 'https:'});
+export default io(HOST, {secure});
